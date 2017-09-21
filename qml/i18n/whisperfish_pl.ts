1e6c8752a98083ea7b762feb578631ba15c70fbc
paytchoo <paytchoo@users.noreply.github.com>
2017-09-21 10:55:27 +0200

    Add Polish Translation, correct minor typos

--- qml/i18n/whisperfish_pl.ts
+++ qml/i18n/whisperfish_pl.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US">
+<TS version="2.1" language="pl_PL">
 <context>
     <name></name>
     <message id="whisperfish-cover-new-label">
