38263513883b8c1d5021e82f5cdaa583815eecc1
paytchoo <paytchoo@users.noreply.github.com>
2017-09-21 10:53:27 +0200

    Correct minor typos

--- qml/i18n/whisperfish_nl.ts
+++ qml/i18n/whisperfish_nl.ts
@@ -253,8 +253,8 @@
     </message>
     <message id="whisperfish-delete-message">
         <location filename="../pages/MessagesView.qml" line="97"/>
-        <source>Deleteing</source>
-        <extracomment>Deleteing message remorse</extracomment>
+        <source>Deleting</source>
+        <extracomment>Deleting message remorse</extracomment>
         <translation>Wordt verwijderd</translation>
     </message>
     <message id="whisperfish-resend-message">
@@ -314,7 +314,7 @@
     <message id="whisperfish-error-verify-contact">
         <location filename="../pages/NewMessage.qml" line="108"/>
         <source>Warning: could not verify contact in Signal</source>
-        <extracomment>Could verify contact is registered with signal</extracomment>
+        <extracomment>Could not verify contact is registered with signal</extracomment>
         <translation>Waarschuwing: kon niet verifiëren dat contact geregistreerd is bij Signal</translation>
     </message>
     <message id="whisperfish-recipient">
